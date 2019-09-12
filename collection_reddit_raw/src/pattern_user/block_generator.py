e4714e1e1b7525ec49721dc3d6ca6d6d985bbc49
frankwang95 <fkwang@uchicago.edu>
2019-09-11 23:45:24 -0700

    Added working user pattern ingestor.

---
+++ collection_reddit_raw/src/pattern_user/block_generator.py
@@ -0,0 +1,112 @@
+from sqlalchemy import create_engine
+import MySQLdb as sql
+import pandas as pd
+import calendar
+
+from lib_learning.collection.base_generator import WorkBlockGenerator
+
+
+def chunk_list(list, chunk_size):
+    return (list[i:i+chunk_size] for i in range(0, len(list), chunk_size))
+
+
+class RedditUserHistoryBlockGenerator(WorkBlockGenerator):
+    def __init__(
+        self, sql_params, submission_table, comment_table,
+        reference_submission_table, reference_comment_table, chunk_size, left_bound
+    ):
+        self.sql_params = sql_params
+        self.submission_table = submission_table
+        self.comment_table = comment_table
+        self.reference_submission_table = reference_submission_table
+        self.reference_comment_table = reference_comment_table
+        self.chunk_size = chunk_size
+        self.left_interval = left_bound
+        self.right_interval = self.get_right_interval()
+
+
+    def get_known_users(self):
+        query = '''
+            SELECT *
+            FROM (
+                SELECT DISTINCT(author_name) FROM {}
+                UNION ALL
+                SELECT DISTINCT(author_name) FROM {}
+            ) joined_table
+        '''.format(self.submission_table, self.comment_table)
+
+        db = sql.connect(**self.sql_params)
+        cur = db.cursor()
+        cur.execute(query)
+        db.commit()
+        result_set = cur.fetchall()
+        cur.close()
+        db.close()
+
+        return set([r[0] for r in result_set if r[0] is not None])
+
+
+    def get_all_users(self):
+        query = '''
+            SELECT DISTINCT(author_name)
+            FROM (
+                (SELECT author_name FROM {})
+                UNION ALL
+                (SELECT author_name FROM {})
+            ) alias
+        '''.format(self.reference_comment_table, self.reference_submission_table)
+        db = sql.connect(**self.sql_params)
+        cur = db.cursor()
+        cur.execute(query)
+        db.commit()
+        result_set = cur.fetchall()
+        cur.close()
+        db.close()
+
+        return set([r[0] for r in result_set if r[0] is not None])
+
+
+    def get_right_interval(self):
+        query = '''
+            SELECT
+                MAX(creation_datetime) AS max_dt
+            FROM
+            (
+                SELECT creation_datetime FROM {}
+                UNION ALL
+                SELECT creation_datetime FROM {}
+            ) joined_table
+        '''.format(self.submission_table, self.comment_table)
+        db = sql.connect(**self.sql_params)
+        cur = db.cursor()
+        cur.execute(query)
+        db.commit()
+        result_set = cur.fetchall()
+        cur.close()
+        db.close()
+
+        if result_set[0][0] is None:
+            return self.left_interval
+        return calendar.timegm(result_set[0][0].timetuple())
+
+
+    def get_next(self, interval):
+        assert interval < 24 * 60 * 60
+        new_right_interval = self.right_interval + interval
+        all_users = self.get_all_users()
+        known_users = self.get_known_users()
+
+        new_user_work_packets = [{
+            'username': ','.join(wp),
+            'after': self.left_interval,
+            'before': new_right_interval,
+        } for wp in chunk_list(list(all_users - known_users), self.chunk_size)]
+
+        old_user_work_packets = [{
+            'username': ','.join(wp),
+            'after': self.right_interval,
+            'before': new_right_interval,
+        } for wp in chunk_list(list(known_users), self.chunk_size)]
+
+        self.right_interval = new_right_interval
+        return new_user_work_packets + old_user_work_packets
--- collection_reddit_raw/src/pattern_user/worker.py
+++ collection_reddit_raw/src/pattern_user/worker.py
@@ -1,34 +1,56 @@
 import praw
 import collection_reddit_raw.src.psraw as psraw
+import prawcore
 
 from lib_borrowbot_core.raw_objects.submission import Submission
 from lib_borrowbot_core.raw_objects.comment import Comment
+from lib_learning.collection.base_worker import Worker
 from collection_reddit_raw.src.writers.submission_writer import SubmissionWriter
 from collection_reddit_raw.src.writers.comment_writer import CommentWriter
 
 
-class RedditUserHistoryTask(object):
-    def __init__(self, logger, sql_params, reddit_params):
-        self.sql_params = sql_params
-        self.logger = logger
-        self.reddit_params = reddit_params
-        self.sql_params = sql_params
+class RedditUserHistoryWorker(Worker):
+    def __init__(self, interface, logger, submission_table, comment_table, sql_params, reddit_params):
+        super().__init__(interface, self.main, logger)
 
-        self.reddit = praw.Reddit(**reddit_params)
-        # self.comment_writer = CommentWriter(logger, sql_params, float('inf'))
-        # self.submission_writer = SubmissionWriter(logger, sql_params, float('inf'))
+        self.sql_params = sql_params
+        self.reddit_params = reddit_params
+        self.submission_table = submission_table
+        self.comment_table = comment_table
+
+        self.reddit = praw.Reddit(**self.reddit_params)
+        self.comment_writer = CommentWriter(self.logger, self.sql_params, self.comment_table, 64)
+        self.submission_writer = SubmissionWriter(self.logger, self.sql_params, self.submission_table, 64)
+
 
     def main(self, block):
+        assert 'username' in block
+        assert 'after' in block
+        assert 'before' in block
+
         submission_iterator = psraw.submission_search(
-            self.reddit, author='franklywang', limit=30, sort='asc', after=1483228800
+            self.reddit, author=block['username'], limit=100000, sort='asc', after=block['after'], before=block['before']
         )
         for submission in submission_iterator:
-            s = Submission(submission)
-            print(s.creation_datetime)
+            # Try block needed to exclude errors submissions from private subreddits
+            try:
+                s = Submission(submission)
+            except (prawcore.exceptions.NotFound, prawcore.exceptions.Forbidden):
+                self.logger.warning("parsing submission {} failed due to 403 or 404".format(submission.id))
+                continue
+            self.submission_writer.push(s)
 
         comment_iterator = psraw.comment_search(
-            self.reddit, author='franklywang', limit=30, sort='asc', after=1483228800
+            self.reddit, author=block['username'], limit=100000, sort='asc', after=block['after'], before=block['before']
         )
         for comment in comment_iterator:
-            c = Comment(comment)
-            print(c.creation_datetime)
+            # Try block needed to exclude errors comments from private subreddits
+            try:
+                c = Comment(comment)
+            except (prawcore.exceptions.NotFound, prawcore.exceptions.Forbidden):
+                self.logger.warning("parsing comment {} failed due to 403 or 404".format(comment.id))
+                continue
+            self.comment_writer.push(c)
+
+        self.submission_writer.flush()
+        self.comment_writer.flush()
